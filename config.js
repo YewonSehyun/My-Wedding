b92fc146e3c38a045fcdd2825c535e40145fb54c
YewonSehyun <sia_z3@naver.com>
2026-05-10 15:17:45 +0900

    Update config.js

--- config.js
+++ config.js
@@ -64,17 +64,17 @@ const CONFIG = {
   // (mapLinks는 wedding 객체 내에 포함)
 
   // ── 마음 전하실 곳 ──
-  accounts: {
-    groom: [
-      { role: "신랑", name: "이세현", bank: "국민은행", number: "454-101-01018871" },
-      { role: "아버지", name: "이용욱", bank: "신한은행", number: "000-000-000000" },
-      ]
-  },
-    bride: [
-      { role: "신부", name: "김예원", bank: "신한은행", number: "110-233-760140" },
-      { role: "어머니", name: "이유진", bank: "신한은행", number: "110-158-398614" }
-    ]
-  },
+ accounts: {
+  groom: [
+    { role: "신랑", name: "이세현", bank: "국민은행", number: "454-101-01018871" },
+    { role: "아버지", name: "이용욱", bank: "신한은행", number: "000-000-000000" }
+  ],
+
+  bride: [
+    { role: "신부", name: "김예원", bank: "신한은행", number: "110-233-760140" },
+    { role: "어머니", name: "이유진", bank: "신한은행", number: "110-158-398614" }
+  ]
+},
 
   // ── 링크 공유 시 나타나는 문구 ──
   meta: {
